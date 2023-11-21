506df242accdf2576a478a1d6b2646c1ea270e08
victo-or <vic_cham@hotmail.com>
2023-11-21 12:33:33 -0500

    Petites corrections: pointer, une saq, svg chevron

--- resources/views/cellier/index.blade.php
+++ resources/views/cellier/index.blade.php
@@ -30,7 +30,7 @@
                         </div>
                     </div>
                     <svg class="card-arrow-icon" width="17" height="32" viewBox="0 0 17 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M1 1L16 16L1 31" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M1 1L16 16L1 31" stroke="black" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
                 </div>
             </a>
