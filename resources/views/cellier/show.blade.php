9a08dfaf29f366340d82e21f3b6591e503ca4ab1
SarahFergusonPoirier <sarahfergusonpoirier@hotmail.com>
2023-11-22 11:24:17 -0500

    Creation de la liste et du cellier favoris, noms des celliers et des listes uniques pour chaque utilisateur

--- resources/views/cellier/show.blade.php
+++ resources/views/cellier/show.blade.php
@@ -12,12 +12,14 @@
     </header>
     <main class="nav-margin">
         <h1 class="btn-modify">{{ $cellier->nom }}
-            <a href="{{ route('cellier.edit', $cellier->id) }}">
-                <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M19 15V19C19 19.5304 18.7893 20.0391 18.4142 20.4142C18.0391 20.7893 17.5304 21 17 21H3C2.46957 21 1.96086 20.7893 1.58579 20.4142C1.21071 20.0391 1 19.5304 1 19V5C1 4.46957 1.21071 3.96086 1.58579 3.58579C1.96086 3.21071 2.46957 3 3 3H7" stroke="#3B3B3B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M11.5 14.8L21 5.2L16.8 1L7.3 10.5L7 15L11.5 14.8Z" stroke="#3B3B3B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
-            </a>
+            @if ($cellier->nom != 'Favoris')
+                <a href="{{ route('cellier.edit', $cellier->id) }}">
+                    <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M19 15V19C19 19.5304 18.7893 20.0391 18.4142 20.4142C18.0391 20.7893 17.5304 21 17 21H3C2.46957 21 1.96086 20.7893 1.58579 20.4142C1.21071 20.0391 1 19.5304 1 19V5C1 4.46957 1.21071 3.96086 1.58579 3.58579C1.96086 3.21071 2.46957 3 3 3H7" stroke="#3B3B3B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M11.5 14.8L21 5.2L16.8 1L7.3 10.5L7 15L11.5 14.8Z" stroke="#3B3B3B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </a>
+            @endif
         </h1>
         <a href="{{ route('ajout.index', ['cellier_id' => $cellier->id] ) }}" class="btn-arrow btn-round">
             Ajouter une bouteille
@@ -95,17 +97,17 @@
         </a>
         <div class="card-count">
             <p>                
-                @if($cellier->bouteillesPersonnaliseesCelliers->count() > 0)
+                {{--@if($cellier->bouteillesPersonnaliseesCelliers->count() > 0)
                     {{ $cellier->quantiteTotalPersonnalisee }} bouteille(s)
                 @else 
                     Aucune bouteille
-                @endif
+                @endif--}}
             </p>
         </div>
         @foreach($cellier->bouteillesPersonnaliseesCelliers as $bouteillesPersonnaliseesCelliers)
         <section class="card-bouteille" id="{{ $bouteillesPersonnaliseesCelliers->id }}" data-location="celliers">
             <picture>
-                <img src="" alt="{{ $bouteillesPersonnaliseesCelliers->bouteillePersonnalisee->nom }}">
+                <img src="{{ asset('assets/img/bouteille_personnalisee.webp') }}" alt="{{ $bouteillesPersonnaliseesCelliers->bouteillePersonnalisee->nom }}">
             </picture>
             <div class="card-bouteille-content">
                 <div class="card-bouteille-info">
