a6e1b72e3d92439d236f89bd50f6233e259ebc57
ElodieBou44 <elodieboulanger4@outlook.com>
2023-11-23 07:34:27 -0500

    Rectification des tabs dans email.mail et dans email.problem

--- resources/views/email/mail.blade.php
+++ resources/views/email/mail.blade.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vino - Réinitialisation de mot de passe</title>
-</head>
-<body>
-    Bonjour {{ $name }},
-    <p>
-        Il semble que vous avez oublié votre mot de passe.
-        {!! $body !!}
-    </p>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Vino - Réinitialisation de mot de passe</title>
+    </head>
+    <body>
+        Bonjour {{ $name }},
+        <p>
+            Il semble que vous avez oublié votre mot de passe.
+            {!! $body !!}
+        </p>
+    </body>
 </html>
